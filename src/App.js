d3b2c61e22cf2cf3f683e9ac97aad8cbe3cc679b
KJW-623 <kimjiwoo4056@gmail.com>
2026-03-15 07:36:46 +0900

    배포 준비 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>HANI & JUNI Start!</h1>
+      {/* 나중에 여기에 Intro, Main 페이지들을 연결할 거예요 */}
+      <h1 style={{ color: 'white', textAlign: 'center', marginTop: '20vh' }}>
+        Hani & Juni 1st Anniversary
+      </h1>
+      <p style={{ color: 'rgba(255,255,255,0.7)', textAlign: 'center' }}>
+        성공적으로 배포되었습니다!
+      </p>
     </div>
   );
 }
